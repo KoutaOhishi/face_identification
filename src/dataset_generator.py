bc3d2d13700bcc4c2abe0ff21feab09d755ea4cd
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-02-22 19:14:04 +0900

    絶対パスを取得するように変更

--- src/dataset_generator.py
+++ src/dataset_generator.py
@@ -1,43 +1,35 @@
 #!/usr/bin/env python
 #coding:utf-8
+import os
 import cv2
 import dlib
 import numpy as np
-cap = cv2.VideoCapture(0)
 
-cascade_path = "/home/macbook-air/opencv/data/haarcascades/haarcascade_frontalface_alt.xml"
-cascade = cv2.CascadeClassifier(cascade_path)
+REPOSITORY_PATH = str("/".join(os.path.abspath(__file__).split("/")[0:-2]))
+CASCADE_PATH = REPOSITORY_PATH + "/model/haarcascade_frontalface_alt.xml"
+MODEL_PATH = REPOSITORY_PATH + "/model/shape_predictor_68_face_landmarks.dat"
 
-model_path = "/home/macbook-air/face_identification/model/shape_predictor_68_face_landmarks.dat"
-predictor = dlib.shape_predictor(model_path)
-detector = dlib.get_frontal_face_detector()
+SAVE_FILE_DIR = REPOSITORY_PATH + "/dataset/"
+SAVE_FILE_NAME = "test.csv"
 
-face_landmarks = []
+DATA_NUM = 100
 
-#image_file_dir = "/home/macbook-air/face_identification/images/carlos/"
-image_file_dir = "/home/macbook-air/face_identification/images/rowan/"
+def camera():
+	cap = cv2.VideoCapture(0)
+	cascade = cv2.CascadeClassifier(CASCADE_PATH)
+	predictor = dlib.shape_predictor(MODEL_PATH)
+	detector = dlib.get_frontal_face_detector()
 
-save_file_dir = "/home/macbook-air/face_identification/dataset/"
-#save_file_path = save_file_dir + "carlos.csv"	
-save_file_path = save_file_dir + "rowan.csv"	
+	face_landmarks = []
 
-for n in range(10):
-	#image_file_name = "carlos"+str(n)+".jpeg"
-	image_file_name = "rowan"+str(n)+".jpeg"	
-	raw_img = cv2.imread(image_file_dir+image_file_name)
-	#_, img = cap.read()
-	#original_height, original_width = raw_img.shape[:2]
-	original_width, original_height = raw_img.shape[:2]
-	multiple_list = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
-	for m in multiple_list:	
-		size = (int(original_height*m), int(original_width*m))
-		img = cv2.resize(raw_img, size)
+	while True:
+		_, img = cap.read()
 
 		gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-		faces = cascade.detectMultiScale(gray_img)
+		faces = cascade.detectMultiScale(gray_img, minSize=(30, 30))
 
-		flag = False
 
+		print len(face_landmarks)
 		if len(faces) != 0:
 			for(x, y, width, height) in faces:
 				cv2.rectangle(img, (x, y), (x+width, y+height), (0, 0, 255), 1)
@@ -45,8 +37,8 @@ for n in range(10):
 				landmarks = []
 				for rect in rects:
 					landmarks.append(np.array([[p.x, p.y] for p in predictor(gray_img, rect).parts()]))
-	
-				for landmark in landmarks:	
+
+				for landmark in landmarks:
 					face_landmark = []
 					for i in range(len(landmark)):
 						cv2.drawMarker(img, (landmark[i][0], landmark[i][1]), (21, 255, 12))
@@ -56,34 +48,69 @@ for n in range(10):
 						face_landmark.append(landmark_y)
 					#face_landmark.append("\n")
 					face_landmarks.append(np.array(face_landmark).flatten())
-					flag = True
+
+		if len(face_landmarks) > DATA_NUM:
+			break
+
+		cv2.imshow("viewer", img)
+		key = cv2.waitKey(1)
+
+	cap.release()
+	cv2.destroyAllWindows()
+
+	print "finish"
+	print len(face_landmarks)
+	print len(face_landmarks[0])
+	np_dataset = np.array(face_landmarks)
+	np.savetxt(SAVE_FILE_DIR+SAVE_FILE_NAME, np_dataset)
 
 
-		if flag == False:
-			print n
-			print m
-			print "---"
+
+def image_file():
+	face_landmarks = []
+
+	image_file_dir = REPOSITORY_PATH + "/images/rowan/"
+
+	for n in range(10):
+		#image_file_name = "carlos"+str(n)+".jpeg"
+		image_file_name = "rowan"+str(n)+".jpeg"
+		raw_img = cv2.imread(image_file_dir+image_file_name)
+
+		original_width, original_height = raw_img.shape[:2]
+		multiple_list = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0]
+		for m in multiple_list:
+			size = (int(original_height*m), int(original_width*m))
+			img = cv2.resize(raw_img, size)
+
+			gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+			faces = cascade.detectMultiScale(gray_img)
+
+			if len(faces) != 0:
+				for(x, y, width, height) in faces:
+					cv2.rectangle(img, (x, y), (x+width, y+height), (0, 0, 255), 1)
+					rects = detector(gray_img, 1)
+					landmarks = []
+					for rect in rects:
+						landmarks.append(np.array([[p.x, p.y] for p in predictor(gray_img, rect).parts()]))
+
+					for landmark in landmarks:
+						face_landmark = []
+						for i in range(len(landmark)):
+							cv2.drawMarker(img, (landmark[i][0], landmark[i][1]), (21, 255, 12))
+							landmark_x = (landmark[i][0]-x)*100.00/width
+							landmark_y = (landmark[i][1]-y)*100.00/height
+							face_landmark.append(landmark_x)
+							face_landmark.append(landmark_y)
+						#face_landmark.append("\n")
+						face_landmarks.append(np.array(face_landmark).flatten())
+
+	print "finish"
+	print len(face_landmarks)
+	print len(face_landmarks[0])
+	np_dataset = np.array(face_landmarks)
+	np.savetxt(SAVE_FILE_DIR+SAVE_FILE_NAME, np_dataset)
 
 
-		
-	
-	
-
-		
-		
-	
-		#cv2.imshow("viewer", img)
-		#key = cv2.waitKey(100)
-	
-	#if key == 27:
-	#	break
-
-cap.release()
-cv2.destroyAllWindows()
-
-print "finish"
-print len(face_landmarks)
-print len(face_landmarks[0])
-np_dataset = np.array(face_landmarks)
-np.savetxt(save_file_path, np_dataset)
+if __name__ == "__main__":
+	camera()
 
--- src/face_landmark.py
+++ src/face_landmark.py
@@ -1,44 +1,48 @@
 #!/usr/bin/env python
 #coding:utf-8
+import os
 import cv2
 import dlib
 import numpy as np
-cap = cv2.VideoCapture(0)
 
-cascade_path = "/home/macbook-air/opencv/data/haarcascades/haarcascade_frontalface_alt.xml"
-cascade = cv2.CascadeClassifier(cascade_path)
+REPOSITORY_PATH = str("/".join(os.path.abspath(__file__).split("/")[0:-2]))
+CASCADE_PATH = REPOSITORY_PATH + "/model/haarcascade_frontalface_alt.xml"
+MODEL_PATH = REPOSITORY_PATH + "/model/shape_predictor_68_face_landmarks.dat"
 
-model_path = "/home/macbook-air/face_identification/model/shape_predictor_68_face_landmarks.dat"
-predictor = dlib.shape_predictor(model_path)
-detector = dlib.get_frontal_face_detector()
+def main():
+	cap = cv2.VideoCapture(0)
+	cascade = cv2.CascadeClassifier(CASCADE_PATH)
+	predictor = dlib.shape_predictor(MODEL_PATH)
+	detector = dlib.get_frontal_face_detector()
 
-while True:
-	_, img = cap.read()
-	
-	gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-	faces = cascade.detectMultiScale(gray_img, minSize=(30, 30))
-	
-	if len(faces) != 0:
-		for(x, y, width, height) in faces:
-			cv2.rectangle(img, (x, y), (x+width, y+height), (0, 0, 255), 1)
-			rects = detector(gray_img, 1)
-			landmarks = []
-			for rect in rects:
-				landmarks.append(np.array([[p.x, p.y] for p in predictor(gray_img, rect).parts()]))
-		
-		for landmark in landmarks:	
-			for points in landmark:
-				cv2.drawMarker(img, (points[0], points[1]), (21, 255, 12))
-		
-		
-		
-		
-	
-	cv2.imshow("viewer", img)
-	key = cv2.waitKey(1)
-	
-	if key == 27:
-		break
+	while True:
+		_, img = cap.read()
 
-cap.release()
-cv2.destroyAllWindows()
+		gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+		faces = cascade.detectMultiScale(gray_img, minSize=(30, 30))
+
+		if len(faces) != 0:
+			for(x, y, width, height) in faces:
+				cv2.rectangle(img, (x, y), (x+width, y+height), (0, 0, 255), 1)
+				rects = detector(gray_img, 1)
+				landmarks = []
+				for rect in rects:
+					landmarks.append(np.array([[p.x, p.y] for p in predictor(gray_img, rect).parts()]))
+
+			for landmark in landmarks:
+				for points in landmark:
+					cv2.drawMarker(img, (points[0], points[1]), (21, 255, 12))
+
+
+		cv2.imshow("viewer", img)
+		key = cv2.waitKey(1)
+
+		if key == 27:
+			break
+
+	cap.release()
+	cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+	main()
+
